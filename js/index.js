065b9f73c5f3b99ed52bc44312bf529ff47f56c3
Johan Karlsson <johan.mikael.karlsson@gmail.com>
2015-09-26 01:40:38 +0200

    Adding resize with debounce

--- js/index.js
+++ js/index.js
@@ -112,3 +112,62 @@ gui.add(options, "reset").name("Reset");
 
 reset();
 run();
+
+// Debounce copied from Underscore.js
+
+// Underscore.js 1.8.3
+// http://underscorejs.org
+// (c) 2009-2015 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+// Underscore may be freely distributed under the MIT license.
+
+
+// A (possibly faster) way to get the current timestamp as an integer.
+var now = Date.now || function() {
+  return new Date().getTime();
+};
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+var debounce = function (func, wait, immediate) {
+  var timeout, args, context, timestamp, result;
+
+  var later = function() {
+    var last = now() - timestamp;
+
+    if (last < wait && last >= 0) {
+      timeout = setTimeout(later, wait - last);
+    } else {
+      timeout = null;
+      if (!immediate) {
+        result = func.apply(context, args);
+        if (!timeout) context = args = null;
+      }
+    }
+  };
+
+  return function() {
+    context = this;
+    args = arguments;
+    timestamp = now();
+    var callNow = immediate && !timeout;
+    if (!timeout) timeout = setTimeout(later, wait);
+    if (callNow) {
+      result = func.apply(context, args);
+      context = args = null;
+    }
+
+    return result;
+  };
+};
+// end Underscore.js
+  
+var resize = debounce(function() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  reset();
+}, 250);
+
+window.addEventListener("resize", resize);
+  
